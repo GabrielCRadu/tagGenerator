c7b887d0a9ecd4c81534752bec1a61474957adba
GabrielCRadu <100233959+GabrielCRadu@users.noreply.github.com>
2024-05-09 21:07:10 +0300

    Update install_spacy_models.py

--- install_spacy_models.py
+++ install_spacy_models.py
@@ -1,11 +1,8 @@
 import subprocess
 
-# Defineți comenzile pentru descărcarea modelelor SpaCy pentru limba engleză și română
-commands = [
-    "python -m spacy download en_core_web_md",
-    "python -m spacy download ro_core_news_md"
-]
+# Instalați pachetele necesare folosind pip
+subprocess.run(["pip", "install", "spacy==3.7.4", "langdetect", "firebase_admin"])
 
-# Rulați fiecare comandă folosind subprocess
-for command in commands:
-    subprocess.run(command, shell=True)
+# Descărcați modelele de limbă pentru engleză și română folosind spaCy
+subprocess.run(["python", "-m", "spacy", "download", "en_core_web_md"])
+subprocess.run(["python", "-m", "spacy", "download", "ro_core_news_md"])
